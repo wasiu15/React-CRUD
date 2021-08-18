56740aaea51da4a1bf7734078c052366783cadc4
wasiu15 <crownprincewadas>
2021-08-19 03:09:02 +0800

    update

--- src/reducers/dCandidate.js
+++ src/reducers/dCandidate.js
@@ -2,6 +2,7 @@ import { ACTION_TYPES } from "../actions/dCandidate";
 const initialState = {
   list: [],
 };
+var i = 9;
 export const dCandidate = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_TYPES.FETCH_ALL:
